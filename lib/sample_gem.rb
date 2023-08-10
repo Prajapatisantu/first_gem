7ff2652ed26b30dd3d874028c861102f92d76881
santu essence <santu.essence@gmail.com>
2023-08-10 14:26:21 +0530

    test code

--- lib/sample_gem.rb
+++ lib/sample_gem.rb
@@ -5,4 +5,9 @@ require_relative "sample_gem/version"
 module SampleGem
   class Error < StandardError; end
   # Your code goes here...
+  class << self
+    def welcome
+      puts "hello rails world"
+    end
+  end
 end
